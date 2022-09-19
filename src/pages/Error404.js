7c3c61d0b29fb010351544ad674c3d22cf433511
IngSaray18 <pedrosaray18@hotmail.com>
2022-09-18 23:39:15 -0500

    yes

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,7 @@
 const Error404 = () => {
     const view = `
       <div class="Error404">
-  <img src="../../assets/error-4044.jpg" alt="404 Page Not Found">      </div>
+  <img src="https://retouchcenter.org/wp-content/uploads/2019/04/error-4044.jpg" alt="404 Page Not Found">      </div>
     `;
     return view;
   };
--- src/templates/Header.js
+++ src/templates/Header.js
@@ -4,7 +4,7 @@ const  Header = () =>{
         <div class="Header-logo">
             
                 <a href="/">
-                    <img class="logo-img" src="../../assets/Rick-and-Morty.png" alt="logo rick y morty">
+                    <img class="logo-img" src="https://www.pngitem.com/pimgs/m/25-258834_rick-and-morty-logo-and-image-rick-and.png" alt="logo rick y morty">
                 </a>
             
             </div>
